922f5ccdf499637e7c3ef756eb3ae82ecc4890ad
James McKee <james.mckee.jr@gmail.com>
2021-05-19 22:58:49 -0500

    Adding the sponsor pages

--- src/helperscripts/generatesponsors.py
+++ src/helperscripts/generatesponsors.py
@@ -5,6 +5,7 @@ import json
 import uuid
 
 from datetime import datetime
+import base64
 
 sponsors = glob.glob('data/sponsor/**/*', recursive=True)
 
@@ -27,6 +28,30 @@ output = {
 for spon in current_sponsors:
     print(spon['completed_date'])
     dt = datetime.fromisoformat(spon['completed_date'])
+
+    body = f"""
+
+    <div class="sponsor-image">
+        <img src="/img/sponsor/{spon['image']}" />
+    </div>
+
+    <div class="sponsor-note">
+        We would like to welcome our sponsor {spon['name']} at the {spon['level']} level. {spon['note']}
+    </div>
+    <br />
+    <br />
+
+    <div class="sponsor-website">
+        <strong>Website: </strong> <a href="{spon['web']}">{spon['web']}</a>
+    </div>
+
+    <div class="sponsor-twitter">
+        <strong>Twitter: </strong> <a href="https://www.twitter.com/{spon['twitter'].replace('@','')}">{spon['twitter']}</a>
+    </div>
+    
+
+    """
+
     message = {
         'id': spon['id'],
         'fromfieldFriendly': 'Sponsorship Services',
@@ -34,10 +59,13 @@ for spon in current_sponsors:
         'shortdate': dt.strftime("%a %b %d"),
         'longdate': dt.strftime("%A %B %d, %Y %-I:%M %p"),
         'date': dt.strftime("%Y-%m-%d %H:%M:%S.%f"),
-        'subject': f"{spon['level']} level sponsor - {spon['name']}"
+        'subject': f"{spon['level']} level sponsor - {spon['name']}",
+        'message': base64.encodebytes(body.encode()).decode()
 
     }
 
     output['mailbox'].append(message)
 
-print(output)
+with open('themes/bsb2021/static/data/sponsor.json','w') as ofile:
+    ofile.write(json.dumps(output))
+    ofile.close()
