1e16ef619a0d7b81900f746d1f86621a33b1644b
James McKee <james.mckee.jr@gmail.com>
2021-05-19 22:29:37 -0500

    Checkpointing with working generation for sponsors.

--- src/helperscripts/generatesponsors.py
+++ src/helperscripts/generatesponsors.py
@@ -4,6 +4,8 @@ import glob
 import json
 import uuid
 
+from datetime import datetime
+
 sponsors = glob.glob('data/sponsor/**/*', recursive=True)
 
 current_sponsors = []
@@ -23,15 +25,19 @@ output = {
 
 # Build the sponsors feed
 for spon in current_sponsors:
+    print(spon['completed_date'])
+    dt = datetime.fromisoformat(spon['completed_date'])
     message = {
-        'id': uuid.uuid4(),
+        'id': spon['id'],
         'fromfieldFriendly': 'Sponsorship Services',
         'fromfield': 'sponsorship@bsidesboulder.org',
-        'shortdate': '',
-        'longdate': '',
-        'date': '',
+        'shortdate': dt.strftime("%a %b %d"),
+        'longdate': dt.strftime("%A %B %d, %Y %-I:%M %p"),
+        'date': dt.strftime("%Y-%m-%d %H:%M:%S.%f"),
         'subject': f"{spon['level']} level sponsor - {spon['name']}"
 
     }
 
-    output['mailbox'].append()
+    output['mailbox'].append(message)
+
+print(output)
